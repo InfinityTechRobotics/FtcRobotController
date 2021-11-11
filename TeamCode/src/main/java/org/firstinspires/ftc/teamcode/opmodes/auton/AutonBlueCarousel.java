a1d8e7ba33b09646627a6c8456eab418110253f0
Eric Suydam <eric.suydam@gmail.com>
2021-11-11 15:21:18 -0500

    Added Auton Modes for Red and Blue positions

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/auton/AutonBlueCarousel.java
@@ -0,0 +1,127 @@
+
+package org.firstinspires.ftc.teamcode.opmodes.auton;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.teamcode.hardware.Robot;
+
+/**
+    This Op Mode is for use when part of the BLUE ALLIANCE and we take the position to the
+    SOUTH (closest to the audience and the carousel)
+
+    Path is:
+        1. Drive Forward X inches
+        2. Rotate -90 degrees
+        3. Drive Backward X inches until the robot is touching the front wall
+        4. Strafe left X inches until the robot is touching the Blue Carousel
+        5. Rotate the WOF X rotations to deliver the duck
+        6. Strafe right X inches until the robot is fully inside the BLUE STORAGE UNIT
+        7. Stop
+
+        This would result in 10 + 6 + 2 = 18 points if executed correctly
+ */
+
+@Autonomous(name="Auton BLUE CAROUSEL", group="Widebot")
+
+public class AutonBlueCarousel extends LinearOpMode {
+
+    /* Declare OpMode members. */
+    Robot robot = new Robot();
+    private ElapsedTime     runtime = new ElapsedTime();
+
+    static final double     FORWARD_SPEED = 0.3;
+    static final double     TURN_SPEED    = 0.3;
+
+    String telemetryMessage = "";
+    boolean active = false;
+
+    @Override
+    public void runOpMode() {
+
+        /*
+         * Initialize the drive system variables.
+         * The init() method of the hardware class does all the work here
+         */
+        robot.init(hardwareMap, telemetry);
+
+        // Send telemetry message to signify robot waiting;
+        telemetry.addData("Status", "Ready to run");    //
+        telemetry.update();
+
+        // Wait for the game to start (driver presses PLAY)
+        waitForStart();
+
+        // 1. Drive forward 18 inches
+        robot.Drive(FORWARD_SPEED,18.0);
+
+        while (robot.isMoving) {
+            telemetry.addData("Path", "In Progress");
+            telemetry.update();
+        }
+
+        telemetry.addData("Path", "Complete");
+        telemetry.update();
+
+        // 2. Rotate left 90 degrees to face NORTH
+        robot.Rotate(TURN_SPEED,-90.0);
+
+        while (robot.isMoving) {
+            telemetry.addData("Path", "In Progress");
+            telemetry.update();
+        }
+
+        telemetry.addData("Path", "Complete");
+        telemetry.update();
+
+        // 3. Drive backward 18 inches to touch the front wall
+        robot.Drive(FORWARD_SPEED,-18.0);
+
+        while (robot.isMoving) {
+            telemetry.addData("Path", "In Progress");
+            telemetry.update();
+        }
+
+        telemetry.addData("Path", "Complete");
+        telemetry.update();
+
+        // 4. Strafe left 12 inches until the robot is touching the BLUE CAROUSEL
+        robot.Strafe(FORWARD_SPEED, -12);
+
+        while (robot.isMoving) {
+            telemetry.addData("Path", "In Progress");
+            telemetry.update();
+        }
+
+        telemetry.addData("Path", "Complete");
+        telemetry.update();
+
+        // 5. Spin the Wheel of Fortune
+        robot.rotateWOF(5.0, 0.3);
+
+        while (robot.isWOFMoving()) {
+            telemetry.addData("WOF ", "Rotating");
+            telemetry.update();
+        }
+
+        robot.stopWOF();
+        telemetry.addData("WOF ", "Complete");
+        telemetry.update();
+
+        // 6. Strafe right 12 inches until the robot is completely inside the BLUE STORAGE UNIT
+        robot.Strafe(FORWARD_SPEED, 12);
+
+        while (robot.isMoving) {
+            telemetry.addData("Path", "In Progress");
+            telemetry.update();
+        }
+
+        telemetry.addData("Path", "Complete");
+        telemetry.update();
+
+        // 7. End of Path
+
+    }
+
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/auton/AutonSimpleTest.java
@@ -0,0 +1,112 @@
+
+package org.firstinspires.ftc.teamcode.opmodes.auton;
+import org.firstinspires.ftc.teamcode.hardware.Robot;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+/**
+ * This file illustrates the concept of driving a path based on time.
+ * It uses the common Pushbot hardware class to define the drive on the robot.
+ * The code is structured as a LinearOpMode
+ *
+ * The code assumes that you do NOT have encoders on the wheels,
+ *   otherwise you would use: PushbotAutoDriveByEncoder;
+ *
+ *   The desired path in this example is:
+ *   - Drive forward for 3 seconds
+ *   - Spin right for 1.3 seconds
+ *   - Drive Backwards for 1 Second
+ *   - Stop and close the claw.
+ *
+ *  The code is written in a simple form with no optimizations.
+ *  However, there are several ways that this type of sequence could be streamlined,
+ *
+ * Use Android Studios to Copy this Class, and Paste it into your team's code folder with a new name.
+ * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
+ */
+
+@Autonomous(name="AutonDriveByTimeTest", group="Widebot")
+
+public class AutonSimpleTest extends LinearOpMode {
+
+    /* Declare OpMode members. */
+    Robot robot = new Robot();
+    private ElapsedTime     runtime = new ElapsedTime();
+
+    static final double     FORWARD_SPEED = 0.3;
+    static final double     TURN_SPEED    = 0.3;
+
+    String telemetryMessage = "";
+    boolean active = false;
+
+    @Override
+    public void runOpMode() {
+
+        /*
+         * Initialize the drive system variables.
+         * The init() method of the hardware class does all the work here
+         */
+        robot.init(hardwareMap, telemetry);
+
+        // Send telemetry message to signify robot waiting;
+        telemetry.addData("Status", "Ready to run");    //
+        telemetry.update();
+
+        // Wait for the game to start (driver presses PLAY)
+        waitForStart();
+
+        active = opModeIsActive();
+        // command to go forward 24 inches
+        robot.Drive(FORWARD_SPEED,12.0);
+
+        while (robot.isMoving) {
+            telemetry.addData("Path", "In Progress");
+            telemetry.update();
+        }
+
+        telemetry.addData("Path", "Complete");
+        telemetry.update();
+
+        robot.chill(1000);
+
+        // command to rotate 180 degrees clockwise
+        robot.Rotate(TURN_SPEED, 90.0);
+
+        while (robot.isMoving) {
+            telemetry.addData("Path", "In Progress");
+            telemetry.update();
+        }
+
+        telemetry.addData("Path", "Complete");
+        telemetry.update();
+
+        robot.chill(1000);
+
+        // command to strafe to the right 12 inches
+        robot.Strafe(FORWARD_SPEED,12.0);
+
+        while (robot.isMoving) {
+            telemetry.addData("Path", "In Progress");
+            telemetry.update();
+        }
+
+        telemetry.addData("Path", "Complete");
+        telemetry.update();
+
+        robot.chill(1000);
+
+        // Spin the WOF
+        robot.rotateWOF(5,0.5);
+
+        robot.chill(1000);
+
+        robot.rotateWOF(-5,0.5);
+
+
+    }
+
+}
